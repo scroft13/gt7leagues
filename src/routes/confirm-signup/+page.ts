071921e66411b2bffde40b9cd5123d8ffd941965
Shaun Croft <shaun@redleafsoft.com>
2024-01-27 15:03:36 -0500

    test email signup response 3

--- src/routes/confirm-signup/+page.ts
+++ src/routes/confirm-signup/+page.ts
@@ -1,3 +1,7 @@
+import { supabase } from '$lib/db.js';
+import type { EmailOtpType } from '@supabase/supabase-js';
+// import { redirect } from '@sveltejs/kit';
+
 console.log('now');
 
 /** @type {import('./$types').PageLoad} */
@@ -6,31 +10,81 @@ export async function load({ url }) {
   const fetchUrl = url.searchParams.get('confirmation_url') as string;
   //   const type = url.searchParams.get('type') as string;
   //   const next = url.searchParams.get('next') ?? '/';
-  //   console.log(token_hash);
+  const token = extractTokenFromURL(fetchUrl);
+  const type = extractTypeFromURL(fetchUrl) as EmailOtpType;
+  const email = extractEmailFromURL(fetchUrl) as EmailOtpType;
 
-  const response = await fetch(fetchUrl);
-  console.log(response);
-  //   return response;
+  console.log(token);
+  if (token && type) {
+    const { error } = await supabase.auth.verifyOtp({ token, type, email });
+    if (!error) {
+      console.log('You lucky bastard!');
+    }
+  }
+
+  // Function to extract the value of the 'token' parameter from the URL
+  function extractTokenFromURL(url: string) {
+    // Extract the query string part of the URL
+    const queryString = url.split('?')[1];
+
+    if (queryString) {
+      // Split the query string into individual key-value pairs
+      const queryParams = queryString.split('&');
+
+      // Find the key-value pair where the key is 'token'
+      const tokenParam = queryParams.find((param) => param.startsWith('token='));
+
+      if (tokenParam) {
+        // Extract the value of 'token' from the key-value pair
+        const token = tokenParam.split('=')[1];
+        return token;
+      }
+    }
+
+    // Return null if 'token' parameter is not found
+    return null;
+  }
+
+  function extractTypeFromURL(url: string) {
+    // Extract the query string part of the URL
+    const queryString = url.split('?')[1];
+
+    if (queryString) {
+      // Split the query string into individual key-value pairs
+      const queryParams = queryString.split('&');
+
+      // Find the key-value pair where the key is 'token'
+      const tokenParam = queryParams.find((param) => param.startsWith('type='));
+
+      if (tokenParam) {
+        // Extract the value of 'token' from the key-value pair
+        const token = tokenParam.split('=')[1];
+        return token;
+      }
+    }
+
+    // Return null if 'token' parameter is not found
+    return null;
+  }
+  function extractEmailFromURL(url: string) {
+    // Extract the query string part of the URL
+    const queryString = url.split('?')[1];
+
+    if (queryString) {
+      // Split the query string into individual key-value pairs
+      const queryParams = queryString.split('&');
+
+      // Find the key-value pair where the key is 'token'
+      const tokenParam = queryParams.find((param) => param.startsWith('email='));
+
+      if (tokenParam) {
+        // Extract the value of 'token' from the key-value pair
+        const token = tokenParam.split('=')[1];
+        return token;
+      }
+    }
+
+    // Return null if 'token' parameter is not found
+    return null;
+  }
 }
-
-// import { redirect } from '@sveltejs/kit';
-
-// export const GET = async (event) => {
-// 	const {
-// 		url,
-// 		locals: { supabase }
-// 	} = event;
-// 	const token_hash = url.searchParams.get('token_hash') as string;
-// 	const type = url.searchParams.get('type') as string;
-// 	const next = url.searchParams.get('next') ?? '/';
-
-//   if (token_hash && type) {
-//     const { error } = await supabase.auth.verifyOtp({ token_hash, type });
-//     if (!error) {
-//       throw redirect(303, `/${next.slice(1)}`);
-//     }
-//   }
-
-//   // return the user to an error page with some instructions
-//   throw redirect(303, '/auth/auth-code-error');
-// };
