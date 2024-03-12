6ec1cf74a4dc515168824f975371da29f4a78a0a
Shaun Croft <shaun@redleafsoft.com>
2024-03-12 12:15:58 -0400

    fixed series page

--- src/routes/league/[league_name]/[series]/+page.svelte
+++ src/routes/league/[league_name]/[series]/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import type { League, LeagueSeries } from '$lib/shared';
-  import { storedUser, publicEvents } from '$lib/stores';
   import type { PageData } from './$types';
 
   export let data: PageData;
@@ -9,27 +8,37 @@
   let leagueInfo: League;
   if (data.data) {
     leagueInfo = data.data;
-    let multipleSeries: LeagueSeries[] = leagueInfo.seriesEvents;
-    seriesInfo = multipleSeries.filter((x) => x.name === data.seriesName)[0];
+    let allSeries: LeagueSeries[] = leagueInfo.seriesEvents;
+    seriesInfo = allSeries.filter((x) => x.name === data.seriesName)[0];
   }
 
-  $: console.log($publicEvents, $storedUser, seriesInfo);
+  $: console.log(data.data);
 </script>
 
-<h1>
-  {data.seriesName}
-</h1>
-{#if seriesInfo}
-  <p>
-    {seriesInfo.eventDetails.eventInfo}
-  </p>
-  <ul>
-    League Members
-    <li>
-      {#each seriesInfo.members as member}
-        {member}
-      {/each}
-    </li>
-    <button>Join Series</button>
-  </ul>
-{/if}
+<div class="mx-4 lg:mx-16 mt-4">
+  <div class="flex flex-col items-center lg:items-start justify-center">
+    <p class="text-primary text-xl">
+      {leagueInfo.leagueName}
+    </p>
+    <p class="secondary-text">presents</p>
+    <p class="text-primary text-2xl">
+      {data.seriesName}
+    </p>
+  </div>
+  {#if seriesInfo}
+    <p class="secondary-text mt-6">
+      {seriesInfo.eventDetails.eventInfo}
+    </p>
+    <!-- <ul>
+      League Members
+      <li>
+        {#each seriesInfo.members as member}
+          {member}
+        {/each}
+      </li>
+      <button>Join Series</button>
+    </ul> -->
+  {/if}
+
+  <p class="text-primary text-2xl">Past race results, series standing, and more coming soon!</p>
+</div>
